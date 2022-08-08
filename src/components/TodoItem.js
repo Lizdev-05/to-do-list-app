6105f603e0409850a33e5466b4b0f6f322ae833e
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 14:25:57 +0100

    destructuring was used to replaced the complete, id and other variables

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,20 +10,18 @@ class TodoItem extends Component {
       textDecoration: "line-through",
     };
 
+    const { completed, id, title } = this.props.todo
+
   return (
-  <li className={styles.item}>
+    <li className={styles.item}>
     <input
       type="checkbox"
       className={styles.checkbox}
-      checked={this.props.todo.completed}
-      onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      checked={completed}
+      onChange={() => this.props.handleChangeProps(id)}
     />
-    <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-      Delete
-    </button>
-    <span style={this.props.todo.completed ? completedStyle : null}>
-      {this.props.todo.title}
-    </span>
+    <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+    <span style={completed ? completedStyle : null}>{title}</span>
   </li>
 )
   }
