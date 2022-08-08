1531d0b4bf661abcbae31284a84b83552828880d
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 13:11:43 +0100

    Implement the onSubmit functionalities

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,19 +2,29 @@ import React, { Component } from "react"
 
 class InputTodo extends Component {
   state = {
-    title: ""
-  };
-
+    title: "",
+  }
   onChange = e => {
     this.setState({
-      title: e.target.value
-    });
-  };
+      [e.target.name]: e.target.value,
+    })
+  }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.title);
+  }
+  
   render() {
     return (
-      <form>
-       <input type="text" placeholder="Add todo..." value={this.state.title} onChange={this.onChange}/>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     )
