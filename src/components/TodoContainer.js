422d3f6220cbd5a28e033af847b166be31b91994
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 13:27:18 +0100

    The set state update the state after submission

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -47,11 +47,21 @@ class TodoContainer extends Component {
     });
   };
 
+  addTodoItem = title => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
   render() {
     return (
       <div>
         <Header />
-        <InputTodo />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodoList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
